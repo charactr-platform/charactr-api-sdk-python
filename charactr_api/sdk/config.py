1ffd210534ff1baafee1692415b7a93b43d27211
piotrpcharactr <85494049+piotrpcharactr@users.noreply.github.com>
2023-08-08 12:44:38 +0200

    feat: gemelo rebrand (#6)

--- charactr_api/sdk/config.py
+++ charactr_api/sdk/config.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 
-API_URL = "https://api.charactr.com"
-WS_API_URL = "wss://api.charactr.com"
+API_URL = "https://api.gemelo.ai"
+WS_API_URL = "wss://api.gemelo.ai"
 
 
 class ApiVersion(Enum):
